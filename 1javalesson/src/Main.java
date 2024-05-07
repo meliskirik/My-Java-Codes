3636b884dbfbe05c0a733164339f4c71c7c2a07c
meliskirik <152122959+meliskirik@users.noreply.github.com>
2024-05-07 15:18:46 +0300

    My first codes in JAVA

---
+++ 1javalesson/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		int vize1 = 30;
+//		int vize2 = 60;
+//		int finalnot = 100;
+//		
+//		int sonuc = (vize1 + vize2 + finalnot)/ 3;
+//		
+//		System.out.println(sonuc);
+		
+		int a = 10;
+		a /= 2;
+		System.out.println(a);
+		a *= 3;
+		System.out.println(a);
+	}
+
+}
