8ad1b917969fd38110f031932045c9af88b845f1
meliskirik <152122959+meliskirik@users.noreply.github.com>
2024-05-08 20:52:07 +0300

    I learned to use Scanner

---
+++ 2scanner/src/Test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Adınızı girin: ");
+		String isim = scanner.nextLine();
+		System.out.println("Adınız: " + isim);
+		*/
+	
+		 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("kilonu gir: ");
+		double kilo = scanner.nextDouble();
+		System.out.println("kilonuz: " + kilo);
+	}	
+}
